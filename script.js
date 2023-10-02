75b9e6acf18bcc5cffee8a9b1fe8aa88573715b8
Gabriel Heleno <heleno1000@hotmail.com>
2023-10-02 14:34:34 -0300

    media

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 // h1 principal
     window.sr = ScrollReveal({ reset: true });
 
-    sr.reveal('.principal', {duration: 1000});
+    // home
+    sr.reveal('.h1-home', {duration: 2000});
+    sr.reveal('.h3-home', {duration: 4000});
 
+    // quem-somos
+    //sr.reveal('.quem-somos', {duration: 2000});
 // ------------------ ScrollReveal ---------------------

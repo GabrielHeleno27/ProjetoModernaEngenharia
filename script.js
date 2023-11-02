69a8178e0bc74ad415c278e9a4d226e6be298da3
Gabriel Heleno <heleno1000@hotmail.com>
2023-11-02 16:02:27 -0300

    navbar

--- script.js
+++ script.js
@@ -5,13 +5,12 @@
     // home
     sr.reveal('.home-content', {
         duration: 2000,
-        delay: 500
+        delay: 50
     });
     sr.reveal('.home-image', {
         duration: 2000,
         origin: 'left',
         distance: '70px',
-        delay: 1000
     })
     
 
@@ -19,53 +18,3 @@
     //sr.reveal('.quem-somos', {duration: 2000});
 // ------------------ ScrollReveal ---------------------
 
-// ------------------ Scroll Smooth JS---------------------
-//const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
-//
-//function getDistanceFromTheTop(element) {
-//  const id = element.getAttribute("href");
-//  return document.querySelector(id).offsetTop;
-//}
-//
-// function nativeScroll(distanceFromTheTop) {
-//   window.scroll({
-//     top: distanceFromTheTop,
-//     behavior: "smooth",
-//   });
-// }
-//
-//function scrollToSection(event) {
-//  event.preventDefault();
-//  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
-//  smoothScrollTo(0, distanceFromTheTop);
-//}
-//
-//menuLinks.forEach((link) => {
-//  link.addEventListener("click", scrollToSection);
-//});
-//
-//function smoothScrollTo(endX, endY, duration) {
-//  const startX = window.scrollX || window.pageXOffset;
-//  const startY = window.scrollY || window.pageYOffset;
-//  const distanceX = endX - startX;
-//  const distanceY = endY - startY;
-//  const startTime = new Date().getTime();
-//
-//  duration = typeof duration !== "undefined" ? duration : 400;
-//
-//  const easeInOutQuart = (time, from, distance, duration) => {
-//    if ((time /= duration / 2) < 1)
-//      return (distance / 2) * time * time * time * time + from;
-//    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
-//  };
-//
-//  const timer = setInterval(() => {
-//    const time = new Date().getTime() - startTime;
-//    const newX = easeInOutQuart(time, startX, distanceX, duration);
-//    const newY = easeInOutQuart(time, startY, distanceY, duration);
-//    if (time >= duration) {
-//      clearInterval(timer);
-//    }
-//    window.scroll(newX, newY);
-//  }, 1000 / 60);
-//}
